4e4d3410939b6e207d0b24fcd2fd5327aaff2a09
吕兴海 <814923634@qq.com>
2019-05-13 10:51:55 +0800

    添加函数注释。

--- javaScript-module/module/lib/dom.js
+++ javaScript-module/module/lib/dom.js
@@ -1,8 +1,17 @@
 F.module('module/lib/dom', function() {
     return {
+        /**
+         * 
+         * @param {*} id 元素id
+         */
         g: function(id) {
             return document.getElementById(id);
         },
+        /**
+         * 
+         * @param {*} id 元素id
+         * @param {*} html 要设置的内容
+         */
         html: function(id, html) {
             if (html) {
                 this.g(id).innerHTML = html;
